a5a7d1e61c406e8b94d8d6ced7186f09552f57aa
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-04 18:47:09 -0600

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Eder  Padilla on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

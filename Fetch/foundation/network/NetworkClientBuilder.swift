b70c11c164b44af8b3267c0da6bc3db0e1fdcb0d
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-05 09:04:02 -0600

    Add more error handling

--- Fetch/foundation/network/NetworkClientBuilder.swift
+++ Fetch/foundation/network/NetworkClientBuilder.swift
@@ -14,19 +14,30 @@ extension URLSession {
             throw NetworkError.invalidURL
         }
         
-        let (data, _) = try await URLSession.shared.data(from: url)
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.noInternetConnection
+        }
+        
+        guard httpResponse.statusCode == NetworkError.successCode else {
+            throw NetworkError.invalidStatusCode
+        }
         
         do {
             let decoder = JSONDecoder()
             let responseDecoded = try decoder.decode(T.self, from: data)
             return responseDecoded
         } catch {
-            throw NetworkError.invalidData
+            throw NetworkError.invalidResponse
         }
     }
 }
 
 enum NetworkError: Error {
+    static let successCode = 200
     case invalidURL
-    case invalidData
+    case invalidResponse
+    case invalidStatusCode
+    case noInternetConnection
 }

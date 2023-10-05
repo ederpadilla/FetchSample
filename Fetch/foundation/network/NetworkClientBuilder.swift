52d80b6546527187dae2fe1ec28dab706d89d22b
€%ER Padilla! <eder.padilla97@gmail.com>
2023-10-04 20:01:41 -0600

    Add client builder

---
+++ Fetch/foundation/network/NetworkClientBuilder.swift
@@ -0,0 +1,39 @@
+//
+//  NetworkClientBuilder.swift
+//  Fetch
+//
+//  Created by Eder  Padilla on 04/10/23.
+//
+
+import Foundation
+
+extension URLSession {
+    
+    func fetch<T: Decodable>(endopoint: String, completion: @escaping (Result<T, Error>) -> Void) {
+        guard let url = URL(string: BuildConfig.environment.description + endopoint) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                do {
+                    let decoder = JSONDecoder()
+                    let decodedData = try decoder.decode(T.self, from: data)
+                    completion(.success(decodedData))
+                } catch {
+                    completion(.failure(NetworkError.invalidData))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+enum NetworkError: Error {
+    case invalidURL
+    case invalidData
+}
